0a237bcca75de4a4d8377027c1fa6375cb486d8a
RePod <RePod@users.noreply.github.com>
2015-08-16 18:35:28 -0400

    Various
    Added self-hosting backup includes.
    Partially mirrored Smea's current content with changes to reflect this
    design.
    Basic event handlers for the menu.
    Basic auto-handle the URL if a hash is in it.

--- site.js
+++ site.js
@@ -1,8 +1,14 @@
 //If we ever need it, it's already here.
+//No object literals here, officer.
 
 $(document).ready(function() {
     var UA = navigator.userAgent,
-        THREEDS = (UA.indexOf("3DS") > -1) ? true : false; //Cannot possibly fail.
+        THREEDS = (UA.indexOf("3DS") > -1) ? true : false, //Cannot possibly fail.
+        HASH = window.location.hash;
+
+    //Auto-toggle.
+    if (HASH.length > 0)
+        toggle(HASH);
 
     if (THREEDS) {
         var browser_ver = parseFloat(UA.match(/Version\/([\d\.]+)\./).pop());
@@ -22,8 +28,28 @@ $(document).ready(function() {
             //9.9, most likely.
             $("#m_ironfall").find("img").addClass("good_browser");
         }
+    }
 
+    //Bind selections to toggle content.
+    //Could do this via Bootstrap without additonal code but eh.
+    $("a[id^=m_]").click(function () {
+        var s = "#" + $(this).attr("id").split("_").pop();
 
+        toggle(s);
+    });
 
+    function toggle(s) {
+        //Fancy transitions don't work well because of browser responsiveness.
+
+        if ($(s).length > 0) { //Verify the selector exists so people can't accidentally send garbage through.
+            if (s) {
+                $("#main").hide();
+                $(".method:not(" + s + ")").hide();
+                $(s).show();
+            } else {
+                $("#main").show();
+                $(".method").hide();
+            }
+        }
     }
 });
